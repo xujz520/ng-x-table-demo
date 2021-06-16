ecdebbaa4c23b471fbe86444b3ae75c792ad0947
xujz520 <765550360@qq.com>
2021-06-16 14:49:12 +0800

    2 files modified, 3 added

---
+++ src/app/routes/antdesign/antdesign.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { XTableColumns } from '@ng-dms/x-table';
+
+@Component({
+  selector: 'app-antdesign',
+  templateUrl: './antdesign.component.html',
+  styleUrls: ['./antdesign.component.css']
+})
+export class AntdesignComponent implements OnInit {
+
+  searchParams: any = {};
+
+  @ViewChild('baseTable', { static: true }) baseTable = null;
+  columns: XTableColumns = [
+    { title: '学号', field: 'no', fixed: 'left' },
+    { title: '姓名', field: 'name', fixed: 'left' },
+    { title: '性别', field: 'sex' },
+    { title: '年龄', field: (rowData) => `${rowData.age}岁`  },
+    { title: '学院', field: 'college' },
+    { title: '专业', field: 'major' },
+    { title: '班级', field: (rowData) => `<strong>${rowData.class}</strong>班`  },
+    { title: '入学时间', field: 'entranceTime', pipe: "date" },
+    { title: '奖学金', field: 'scholarship', pipe: "currency", align: 'right' },
+    { title: '社团', field: 'club' },
+    { title: '备注', field: (rowData) => rowData.remark },
+    { title: '操作项', field: '#btn', fixed: 'right', toggleCheckbox: false }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -6,15 +6,17 @@ import { SharedModule } from '../shared/shared.module';
 
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
+import { AntdesignComponent } from './antdesign/antdesign.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'test1' },
   { path: 'test1', component: Test1Component },
   { path: 'test2', component: Test2Component },
+  { path: 'test3', component: AntdesignComponent },
 ];
 
 @NgModule({
-  declarations: [Test1Component, Test2Component],
+  declarations: [Test1Component, Test2Component, AntdesignComponent],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes),
